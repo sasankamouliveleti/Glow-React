460fce3b5650d0d3c873ba68ef857ae8e26bb542
Veleti <svelet2@uic.edu>
2022-04-25 00:35:55 -0500

    Links changed

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar"
 
@@ -14,7 +15,7 @@ function Homepage() {
                         <h5 class="header col s12 light">Your one stop destination for all your skin care questions</h5>
                     </div>
                     <div class="row center">
-                        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a>
+                        <Link to="/SignUp"><a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started</a></Link>
                     </div>
                     <br /><br />
 
@@ -26,7 +27,7 @@ function Homepage() {
                         <div class="col s12 m4">
                             <div class="icon-block">
                                 <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-                                <h5 class="center">Speeds up development</h5>
+                                <h5 class="center">Save money while shopping for skincare products</h5>
 
                                 <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
                             </div>
@@ -35,7 +36,7 @@ function Homepage() {
                         <div class="col s12 m4">
                             <div class="icon-block">
                                 <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-                                <h5 class="center">User Experience Focused</h5>
+                                <h5 class="center">Learn about Skincare</h5>
 
                                 <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
                             </div>
@@ -44,7 +45,7 @@ function Homepage() {
                         <div class="col s12 m4">
                             <div class="icon-block">
                                 <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-                                <h5 class="center">Easy to work with</h5>
+                                <h5 class="center">Be Consistent with your Skincare Routine</h5>
 
                                 <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
                             </div>
